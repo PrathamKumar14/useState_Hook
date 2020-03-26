0fe48c3d055c771a2b2fac99df373a8e30f46dea
Pratham kumar <prathamsolera@gmail.com>
2020-03-26 23:12:34 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//jshint esversion: 6
+
+import React, { useState } from "react";
 
 function App() {
+  setInterval(updateTime, 1000);
+
+  var now = new Date().toLocaleTimeString("en-GB");
+
+  var [time, setTime] = useState(now);
+
+  function updateTime() {
+    setTime(new Date().toLocaleTimeString("en-GB"));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{time}</h1>
     </div>
   );
 }
