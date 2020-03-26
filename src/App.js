128db619f9da07754f1ed2bbe33af83828ecc426
Pratham kumar <prathamsolera@gmail.com>
2020-03-27 00:01:26 +0530

    Add day

--- src/App.js
+++ src/App.js
@@ -3,19 +3,50 @@
 import React, { useState } from "react";
 
 function App() {
+
   setInterval(updateTime, 1000);
 
   var now = new Date().toLocaleTimeString("en-GB");
+  // var day = new Date().getDay();
+  var day = "";
 
-  var [time, setTime] = useState(now);
+  switch (new Date().getDay()) {
+
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+  default:
+
+}
+
+  var [time, setTime] = useState([now, day]);
 
   function updateTime() {
-    setTime(new Date().toLocaleTimeString("en-GB"));
+    setTime([new Date().toLocaleTimeString("en-GB"), day]);
   }
 
   return (
     <div className="container">
-      <h1>{time}</h1>
+      <h1>{time[1]}</h1><br />
+      <h2>{time[0]}</h2>
     </div>
   );
 }
